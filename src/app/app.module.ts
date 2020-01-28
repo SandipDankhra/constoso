f846105070063d4bdc90f54fb69a055f113fe052
Techie Taher <33249157+TechieTaher@users.noreply.github.com>
2020-01-28 17:36:49 +0530

    Contoso template is set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { CourseComponent } from './course/course.component';
+import { SigninComponent } from './user/signin/signin.component';
+import { SignupComponent } from './user/signup/signup.component';
+
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { CourseAddComponent } from './course/course-add/course-add.component';
+import { CourseEditComponent } from './course/course-edit/course-edit.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    CourseComponent,
+    SigninComponent,
+    SignupComponent,
+    UserEditComponent,
+    CourseAddComponent,
+    CourseEditComponent,
+
   ],
   imports: [
     BrowserModule,
